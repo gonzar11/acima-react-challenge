2ee41b817931c0f9081b55a9ba50a0ee04b76f9b
Gonzalo Ricco <gonzalo.ricco1@gmail.com>
2024-05-16 10:37:41 -0300

    Adds Sidebard to main page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,24 @@ import React from "react";
 import { encodedHtml } from "@/data/data";
 import { decodeBase64ToString } from "@/utils/stringUtils";
 import HtmlParser from "@/utils/htmlParser";
+import Sidebar from "@/components/Sidebar";
 
-interface HtmlContentComponentProps {
-  encodedHtml: string;
-}
-
-const HtmlContentComponent: React.FC<HtmlContentComponentProps> = ({
-  encodedHtml,
-}) => {
+const Home: React.FC = () => {
   const parser = HtmlParser(decodeBase64ToString(encodedHtml));
 
-  const bodyContent = parser.extractBodyContent();
-  console.log(parser.extractTotals()); // You had this console.log storing into a constant which isn't used.
-
-  const createMarkup = () => ({ __html: bodyContent });
-
-  return <div dangerouslySetInnerHTML={createMarkup()} />;
+  return (
+    <main className="flex h-screen w-full flex-col">
+      <header className="flex h-16 items-center justify-center bg-gray-900 text-white">
+        <h1 className="text-2xl font-bold">Ledger Calculator</h1>
+      </header>
+      <div className="flex flex-1">
+        <Sidebar htmlParser={parser} />
+        <div className="flex-1 p-8">
+          <h1>Main content here</h1>
+        </div>
+      </div>
+    </main>
+  );
 };
-
-const Home: React.FC = () => (
-  <main className="flex h-screen w-full flex-col">
-    <header className="flex h-16 items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-2xl font-bold">Ledger Calculator</h1>
-    </header>
-    <div className="flex flex-1">
-      <div className="w-64 border-r bg-gray-100 p-6">
-        <HtmlContentComponent encodedHtml={encodedHtml} />
-      </div>
-
-      <div className="flex-1 p-8">
-        <h1>Main content here</h1>
-      </div>
-    </div>
-  </main>
-);
 
 export default Home;
