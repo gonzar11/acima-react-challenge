982742a393ac54a9dc554eab871722d7c26c3434
Gonzalo Ricco <gonzalo.ricco1@gmail.com>
2024-05-16 19:59:38 -0300

    Fixes formating in stringUtils

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,2 +1,2 @@
 export const decodeBase64ToString = (base64String: string): string =>
-  Buffer.from(base64String, 'base64').toString('utf-8');
+  Buffer.from(base64String, "base64").toString("utf-8");
