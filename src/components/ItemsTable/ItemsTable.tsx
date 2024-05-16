7167d8ed6da0d560080b1645782019c66c26f1ed
Gonzalo Ricco <gonzalo.ricco1@gmail.com>
2024-05-16 16:45:38 -0300

    Adds styles to ItemsTable

--- src/components/ItemsTable/ItemsTable.tsx
+++ src/components/ItemsTable/ItemsTable.tsx
@@ -1,34 +1,44 @@
 import React from "react";
-import { parseStyle } from "@/utils/styleUtils";
 import { TableData } from "@/utils/htmlParser";
+import { getClassNames } from "@/utils/styleUtils";
 
 interface ItemsTableProps extends TableData {}
 
-const ItemsTable: React.FC<ItemsTableProps> = ({ head, body, style }) => (
-  <table style={style}>
-    <thead>
-      {head.map((row, rowIndex) => (
-        <tr key={`head-row-${rowIndex}`}>
-          {row.map((cell, cellIndex) => (
-            <th key={`head-cell-${cellIndex}`} style={cell.style}>
-              {cell.content}
-            </th>
-          ))}
-        </tr>
-      ))}
-    </thead>
-    <tbody>
-      {body.map((tableItemRow, rowIndex) => (
-        <tr key={`body-row-${rowIndex}`}>
-          {Object.entries(tableItemRow).map(([key, cell]) => (
-            <td key={key} style={cell.style}>
-              {cell.content}
-            </td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+const ItemsTable: React.FC<ItemsTableProps> = ({ head, body, style }) => {
+  const tableClasses = "mb-8 w-full";
+  const theadCellClasses = "p-2 text-left font-semibold text-gray-800";
+  const tbodClasses =
+    "&>*:nth-child(even)]:bg-gray-50 [&>*:nth-child(odd)]:bg-white [&>*]:border-b";
+  return (
+    <table className={getClassNames(tableClasses, style)} style={style}>
+      <thead>
+        {head.map((row, rowIndex) => (
+          <tr key={`head-row-${rowIndex}`}>
+            {row.map((cell, cellIndex) => (
+              <th
+                key={`head-cell-${cellIndex}`}
+                className={getClassNames(theadCellClasses, cell.style)}
+                style={cell.style}
+              >
+                {cell.content}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody className={tbodClasses}>
+        {body.map((tableItemRow, rowIndex) => (
+          <tr key={`body-row-${rowIndex}`}>
+            {Object.entries(tableItemRow).map(([key, cell]) => (
+              <td key={key} style={cell.style}>
+                {cell.content}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default ItemsTable;
