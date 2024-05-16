211f76aa2010826dccd213d1822e34a206f72052
Gonzalo Ricco <gonzalo.ricco1@gmail.com>
2024-05-16 10:39:59 -0300

    Fixes style error in Title component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { parseStyle } from "@/utils/styleUtils";
 
 interface TitleProps {
   text: string;
-  style: string;
+  style?: React.CSSProperties;
 }
 
 const Title: React.FC<TitleProps> = ({ text, style }) => (
-  <h2 style={{ ...parseStyle(style) }}>{text}</h2>
+  <h2 style={style}>{text}</h2>
 );
 
 export default Title;
